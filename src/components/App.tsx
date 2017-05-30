784bd08e5f477d1180e254202de7dea39497e707
Mohamed Ismat <mohamedismat91@gmail.com>
2017-05-30 21:47:13 +0300

    Refactoring App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,11 +15,6 @@ import {
   deepOrange400
 } from 'material-ui/styles/colors';
 
-import Drawer from 'material-ui/Drawer';
-import {Menu,MenuItem} from 'material-ui/Menu';
-import Divider from 'material-ui/Divider';
-import FontIcon from 'material-ui/FontIcon';
-
 
 import HeaderBar from './HeaderBar';
 import RecipesList from './RecipesList';
@@ -33,7 +28,6 @@ const muiTheme = getMuiTheme({
     accent1Color: deepOrange400
   },
   fontFamily: "Lato, sans-serif"
-  //fontFamily: 'monospace'
 });
 
 let forceNavDown = {'marginTop': '64px'};
@@ -67,35 +61,49 @@ export class App extends React.Component<Props,{}> {
 
         <div style={{height: '100%'}}>
 
-          <Route render={({history})=>
-            <HeaderBar
-              searchKey={this.props.searchKey}
-              changeSearchKey={this.props.changeSearchKey}
-              fetchRecipes={this.props.fetchRecipes}
-              history={history}
+          <Route
+            render={({history})=>
+              <HeaderBar
+                searchKey={this.props.searchKey}
+                changeSearchKey={this.props.changeSearchKey}
+                fetchRecipes={this.props.fetchRecipes}
+                history={history}
               />
-          }/>
+            }
+          />
 
-            <div style={{...forceNavDown, height: '100%'}}>
-              <div className="col-lg-2 visible-lg">
-                <Sidebar
-                  searchFor={key=>this.searchFor(key)}/>
-              </div>
-              <div className="col-md-12 col-lg-10" style={{padding: "48px", height:'100%'} as any}>
-                <Switch>
-                  <Route exact path="/recipes" render={({history})=>(
+
+          <div style={{...forceNavDown, height: '100%'}}>
+
+
+            <div className="col-lg-2 visible-lg">
+              <Sidebar searchFor={key=>this.searchFor(key)}/>
+            </div>
+
+
+            <div className="col-md-12 col-lg-10" style={{padding: "48px", height:'100%'} as any}>
+
+              <Switch>
+
+                <Route
+                  exact path="/recipes"
+                  render={({history})=>(
                     <RecipesList recipes={this.props.searchResults} isSearching={this.props.isSearching} history={history} />
                   )  as any} />
-                  <Route path="/recipes/:id" render={({match})=>(
+
+                <Route
+                  path="/recipes/:id"
+                  render={({match})=>(
                     <RecipeDetails
                       recipe={this.props.selectedRecipe}
                       changeSelectedRecipe={this.props.changeSelectedRecipe}
                       match={match as any} />
                   ) as any}/>
 
-                  </Switch>
-              </div>
+              </Switch>
+
             </div>
+          </div>
 
         </div>
 
